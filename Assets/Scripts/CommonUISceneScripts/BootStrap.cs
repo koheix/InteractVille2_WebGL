7a34150ea0963e1ff032c0c4d3cf35d11122df52
koheix <grapeoxygen@gmail.com>
2026-01-12 15:57:48 +0900

    fix: キーボード入力関連の設定を削除

--- Assets/Scripts/CommonUISceneScripts/BootStrap.cs
+++ Assets/Scripts/CommonUISceneScripts/BootStrap.cs
@@ -6,12 +6,6 @@ public class Bootstrap : MonoBehaviour
 
     private void Start()
     {
-
-// WebGLビルド時のみ、キーボード入力をブラウザに逃がす設定
-#if !UNITY_EDITOR && UNITY_WEBGL
-UnityEngine.WebGLInput.captureAllKeyboardInput = false;
-#endif
-
         // CommonUI を読み込む（永続UI）
         SceneManager.LoadScene("CommonUIScene", LoadSceneMode.Additive);
 
